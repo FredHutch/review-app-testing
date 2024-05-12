ffe944a665a2b945f956b7aaf7835ff515ca4dcb
dtenenba <dtenenba@fhcrc.org>
2024-05-12 13:28:41 -0700

    more experiments

--- src/review_app_testing/app.py
+++ src/review_app_testing/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from review0 branch!"
+    return "Hello World  - (some content that was added in review0 branch)"
 
 if __name__ == "__main__":
     app.run(port=5050, host="0.0.0.0")
